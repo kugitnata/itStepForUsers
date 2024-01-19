89ff950fb7f8fa5280423f72d4157db249d40013
Sergii Getman <sergeygetman2020@gmail.com>
2024-01-19 18:52:09 +0200

    add project for itStepUsers

---
+++ script.js
@@ -0,0 +1,21 @@
+//console.log("some text");
+
+// const toggleModal = (event) => {
+//   event.preventDefault();
+
+//   const getModalStyle = document.getElementById("modal-change");
+//   console.log("getModalStyle", getModalStyle);
+// };
+
+// alert("hi name !!!!");
+
+// confirm("yes", "no");
+
+// prompt("введіть сві зріст");
+
+// const popover = new bootstrap.Popover(
+//   document.querySelector(".example-popover"),
+//   {
+//     container: "body",
+//   }
+// );
